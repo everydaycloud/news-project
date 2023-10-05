b374cf1c94d7aef5c9b97379df328ea69ed926f2
wormrider <126498815+wormrider@users.noreply.github.com>
2023-10-05 16:53:57 +0100

    11 done

--- db/error.controller.js
+++ db/error.controller.js
@@ -1,10 +1,11 @@
 exports.handle500Errors = (err, req, res, next) => {
+  console.log('500 error', err)
   res.status(500).send({ msg: "Internal server error" });
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.msg });
-  }
-  next(err);
+  } else {
+  next(err)}
 };
